ef4ba90c507f4cc76cdf31bf02c80f09eb5721cd
Camilo A <camiloandresarevaloc@hotmail.com>
2020-04-06 15:55:29 -0500

    Funciones sobre condicionales, Busqueda atravez de strings, ordenar y limitar registros

--- gg.sql
+++ gg.sql
@@ -44,6 +44,7 @@ VALUES ('Stephen Edwin', 'King', 'Richard Bachman', 'M', '1947-09-27', 'USA'),
 INSERT INTO AUTORES(NOMBRE, APELLIDO, SEUDONIMO, GENERO, FECHA_NACIMIENTO, PAIS_ORIGEN) -- ESTO ES UNA PRUEBA PARA VER QUE NO SE PODRÀ INGRESAR DATOS DUPLICADOS EN CUANTO AL NOMBRE Y APPELIDO DE UN AUTOR
 VALUES ('Stephen Edwin', 'King', 'Richard Bachman', 'M', '1947-09-27', 'USA');
 
+
 /* INSERTAMOS DATOS EN LA TABALA DE LIBROS */
 
 INSERT INTO LIBROS (AUTOR_ID, TITULO, FECHA_PUBLICACION)
@@ -55,8 +56,10 @@ VALUES (1, 'CARRIE', '1974-01-01'),
        (4, 'mi lucha', '1890-02-23'),
        (5, 'mi lucha', '1997-05-15');
 
-drop table LIBROS;
-drop table AUTORES;
+insert into LIBROS(AUTOR_ID, TITULO, DESCRIPCION, PAGINAS, FECHA_PUBLICACION, VENTAS)
+VALUES (3, 'El Señor De Los Anillos', Null, 10, '1972-05-20', 0);
+
+
 
 /* MODIFICADOR DE TABLAS */ -- LOS MODIFICADORES DE TABLA NOS PERMITEN MODIFICAR, AGREGAR Y ELIMINAR YA SEAN DATOS, TABLAS O COLUMNAS
 
@@ -73,6 +76,7 @@ ALTER TABLE LIBROS
 SELECT *
 FROM LIBROS;
 -- AL VISUALIZAR LA TABALA VEREMOS QUE POR DEFECTO STOCK TIENE EL VALOR DE 10
+
 
 /* SI QUISIERAMOS ELIMINAR UNA COLUMNA TENDRIAMOS */
 
@@ -100,6 +104,7 @@ SELECT *
 FROM LIBROS
 WHERE TITULO = 'carrie';
 -- SOLICITAMOS A LA TABLA LIBRO QUE NOS MUESTRE UN REGISTRO DONDE EL TITULO DE ALGUN DE SUS LIBROS SEA 'CARRIE'
+
 
 /*OPERADORES LOGICOS */
 
@@ -141,12 +146,14 @@ FROM AUTORES
 WHERE SEUDONIMO <=> NULL;
 -- OTRA FORMA DE
 
+
 /* HACER CONSULTA DE REGISTRO MEDIANTE RANGOS */
 
 SELECT TITULO
 FROM LIBROS
 WHERE FECHA_PUBLICACION BETWEEN '1892-01-03' AND '1975-01-01';
 -- TAMBIEN PODEMOS HACER CONSULTAS NO SOLO POR RANGO DE FECHA
+
 
 /* HACER CONSULTA DE REGISTROS UNICOS */
 
@@ -157,6 +164,8 @@ FROM LIBROS;
 
 
 /*ACTUALIZAR REGISTROS*/
+
+
 
 UPDATE LIBROS
 SET TITULO = 'Mi lucha veneca'
@@ -184,52 +193,165 @@ FROM LIBROS;
 -- ¡ALERTA! NO SE USA EL WHERE BORRAREMOS TODA LA TABLA
 
 
- /*ELIMINACION EN CASCADA
+/*ELIMINACION EN CASCADA
 
-   nos permite eliminar datos que se encuentren referenciados en otra tabla es decir datos que sean llaves foraneas
-   en otra tabla pero para ello debemos modificar la tabla original o sea de donde proviene ese dato (si es que tenemos ya la tabla con datos añadidos)
+  nos permite eliminar datos que se encuentren referenciados en otra tabla es decir datos que sean llaves foraneas
+  en otra tabla pero para ello debemos modificar la tabla original o sea de donde proviene ese dato (si es que tenemos ya la tabla con datos añadidos)
+*/
 
-   */
-SHOW CREATE TABLE LIBROS; /* PEDIMOS EUE NOS MUESTR TODA LA INFORMACIÓN DE LA CRAECION DE LA TABLA Y BUSCAMOS EL NOMBRE_CONSTRAINT*/
 
-ALTER TABLE LIBROS DROP  FOREIGN KEY LIBROS_ibfk_1; -- Primero eliminamos la llave foranea de la tabla libros que es donde esta referenciada
+SHOW CREATE TABLE LIBROS; -- PEDIMOS EUE NOS MUESTR TODA LA INFORMACIÓN DE LA CRAECION DE LA TABLA Y BUSCAMOS EL NOMBRE_CONSTRAINT
 
-ALTER TABLE LIBROS ADD FOREIGN KEY (AUTOR_ID) REFERENCES AUTORES (AUTOR_ID) ON DELETE CASCADE; -- luego añadimos de nuevo la llave foranea solo que con un cambio ON DELETE CASCADE; que nos permite eliminar directamente el autor asi este referenciada
+ALTER TABLE LIBROS
+    DROP FOREIGN KEY LIBROS_ibfk_1; -- Primero eliminamos la llave foranea de la tabla libros que es donde esta referenciada
 
-DELETE FROM  AUTORES WHERE AUTOR_ID = 5; -- procedemos a eliminar el autor con id 5 y no nos arrojara un error de identidad referncial porque donde se hace la referencia tiene el ON DELETE CASCADE
+ALTER TABLE LIBROS
+    ADD FOREIGN KEY (AUTOR_ID) REFERENCES AUTORES (AUTOR_ID) ON DELETE CASCADE;
+-- luego añadimos de nuevo la llave foranea solo que con un cambio ON DELETE CASCADE; que nos permite eliminar directamente el autor asi este referenciada
+
+DELETE
+FROM AUTORES
+WHERE AUTOR_ID = 5; -- procedemos a eliminar el autor con id 5 y no nos arrojara un error de identidad referncial porque donde se hace la referencia tiene el ON DELETE CASCADE
 
 desc LIBROS;
 
-/* SI VISUALIZAMOS VEREMOS QUE EL REGISTRO DEL AUTOR NO SE ENCUENTRA Y POR LO TANTO NINGUN LIBRO REGISTRADO QUE LE CORRESPONDA */
+-- SI VISUALIZAMOS VEREMOS QUE EL REGISTRO DEL AUTOR NO SE ENCUENTRA Y POR LO TANTO NINGUN LIBRO REGISTRADO QUE LE CORRESPONDA
 
-SELECT * FROM AUTORES;
-SELECT * FROM LIBROS;
+SELECT *
+FROM AUTORES;
+SELECT *
+FROM LIBROS;
 
 
 /* PARA LA ELIMINACION DE LOS DATOS INCERTADOS EN UNA TABLA */
 
-TRUNCATE TABLE LIBROS; /*A DIFERENCIA DE USAR UN DELETE SIN UN WHERE EL TRUNCATE BORRA TODOS LOS METADATOS ES DECIR NO SE CONTAMINA LA TABLA DE METADATOS ANTIGUOS*/
-
-/* RECURRING EXECUTE */
-
+TRUNCATE TABLE LIBROS;
+-- A DIFERENCIA DE USAR UN DELETE SIN UN WHERE EL TRUNCATE BORRA TODOS LOS METADATOS ES DECIR NO SE CONTAMINA LA TABLA DE METADATOS ANTIGUOS
 
 
-SHOW TABLES;
-DROP DATABASE libreria_cf;
+/* FUNCIONES SOBRE STRINGS */
 
-DROP TABLE AUTORES;
-DROP TABLE LIBROS;
+-- las funciones sobre strings son varias pero tocaremos las más importantes
 
+SELECT CONCAT(NOMBRE, ' ', APELLIDO) as nombre_completo
+FROM AUTORES;
+-- concat nos permite concatenar dos columnas en este caso cuando queremos visualziar el nombre completo de un autor asi que concatenamos nombre y apellido
 
-SHOW DATABASES;
+SELECT LENGTH('caracteres'); -- Length nos retorna la cantidad de caracteres que posee un string contando espacios
+
 SELECT *
 FROM AUTORES
-WHERE SEUDONIMO IS NOT NULL;
+WHERE LENGTH(NOMBRE) > 7; -- tambien podemos usar lenght para hacer consultas con una condicion especifica
 
-DELETE
-FROM AUTORES
+SELECT UPPER(NOMBRE), LOWER(NOMBRE)
+FROM AUTORES; -- upper y lower son funciones pensadas para el formato con upper veremos el string en mayuscula y con lower en miniscula
 
-WHERE AUTOR_ID = 1;
+SELECT TRIM(NOMBRE)
+FROM AUTORES; -- TRIM nos permite eliminar los prefijos y sufijos de un string por default serán los espacios del inicio y el final
+
+SELECT LEFT('Esta es una cadena de caracteres', 5)   as substring_isquierdo,
+       RIGHT('Esta es una cadena de caracteres', 10) as substring_derecho;
+
+/*Left y right estas funciones reciben un string y un entero. Las funciones retornan
+  la cantidad de caracteres que se indican en el entero left contando de isquierda a derecha y right de derecha a isquierda
+ */
 
 
-SELECT NOW();
+SELECT *
+FROM LIBROS
+WHERE LEFT(TITULO, (LENGTH('HarryPotter'))) = 'HarryPotter';
+/*podemos usar estos para realizar consultas pedimos información de todos los libros que empiecen por harrypotter donde
+  la izquierda del titulo (su inicio) cuente con la cantidad de caracteres sean las del titulo a consultar y que estan sean
+  iguales al inicio del titulo que quermos consultar
+ */
+
+
+/*FUNCIONES SOSBRE FECHA   */
+
+SELECT *
+FROM LIBROS
+WHERE CONVERT(FECHA_CREACION, DATE) = '2020-03-27';
+/*podemos realizar consultas filtrando por fechas en este caso por fecha de creación. Debemos tener en cuenta
+ que el tipo de dato es diferente por lo tanto debemos usar connvert para que el retorno del valor al
+  consultar puesto que colcamos un valor con tipo de dato DATE y fecha de creación esta con DATETIME
+*/
+
+
+/*FUNCIONES SOBRE CONDICIONALES*/
+
+SELECT IF(10 > 9, 'es mayor', ' no es mayor');
+-- Select if necesita 3 parametros la condicion y dos valores que retornara en caso de que sea verdadero o falso
+
+-- Se modificará la tabla libros para realizar un ejemplo de ocnsulta con if
+
+update LIBROS
+SET PAGINAS = 0
+WHERE LIBRO_ID BETWEEN 1 AND 4;
+
+-- consulta
+
+SELECT IF(PAGINAS > 0, PAGINAS, 'NO TIENE PAGINAS')
+FROM LIBROS;
+
+-- Realizamos una inserción más en la tabla autores para realizar otro ejemplo del if
+
+INSERT INTO AUTORES(nombre, apellido, seudonimo, genero, fecha_nacimiento, pais_origen)
+    VALUE (  'Alister', 'Crowli',NULL, 'M', '1872-05-23','UK'
+    );
+
+update AUTORES
+set SEUDONIMO = 'El profeta'
+where AUTOR_ID = 7;
+
+SELECT IF(SEUDONIMO is null, ((CONCAT(NOMBRE, ' ', APELLIDO, ' Seudonimo: ', 'No tiene seudonimo'))),
+          (concat(NOMBRE, ' ', APELLIDO, '  Seudonimo: ', SEUDONIMO))) AS NOMBRE_SEUDONIMO
+FROM AUTORES;
+-- Consultamos que autores no tiene seudonimo
+
+
+
+
+/* BUSQUEDAS ATRAVEZ DE STRINGS */
+
+
+/*Realizar una consulta de un substring mediante el uso de LIKE colocaremos el string a buscar con el signo % al inicio
+  si deseamos que esa palabra este al final o al final si deseamos saber si esta al inicio. pero si no sabemos donde esta
+  podremos colcar dos % tanto al inicio como al final
+ */
+SELECT *
+FROM LIBROS
+WHERE TITULO LIKE 'HarryPotter%';
+
+SELECT *
+FROM LIBROS
+WHERE TITULO LIKE '%Anillos';
+
+SELECT *
+FROM LIBROS
+WHERE TITULO LIKE '%De%';
+
+SELECT * FROM LIBROS WHERE TITULO REGEXP '^[EH]';-- Si queremos consultar titulos que comiencen con una letra O con otra podremos usar REGEXP
+
+/* ORDENAR REGISTROS */
+
+/*Ordenar los registros es una manera de manejar un orden en las consultas al colcoar ORDER BY
+  automaticamente al realizar la consulta por defecto este se manerjara de orden ascendente
+  a menos que especifiquemos otro orden por ejemplo de manera descendente desc */
+
+SELECT TITULO FROM LIBROS ORDER BY TITULO ;
+SELECT TITULO FROM LIBROS ORDER BY TITULO desc ;
+
+SELECT LIBRO_ID, TITULO FROM LIBROS ORDER BY LIBRO_ID AND TITULO; -- Tambien podemos ordenar por dos campos como en este caso
+
+/* LIMITAR REGISTROS */
+
+/* Con limit podremos limitar la cantidad de registros que queremos visualizar. Tambien la cantidad
+   de registros pertenecientes a un id o cualquier condicion que establezcamos */
+
+SELECT * FROM LIBROS LIMIT 2;
+
+SELECT * FROM LIBROS WHERE AUTOR_ID = 2 ;
+
+SELECT LIBRO_ID,TITULO,AUTOR_ID FROM LIBROS LIMIT 0, 5; -- Estamos limitando la consulta estableciendo desde cual id empezar hasta cual terminar
+
+
